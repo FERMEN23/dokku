a6e0d2e794ecb1f810204e5b27ce7cd4fdef5785
fernanda <fernandamendoza2300@gmail.com>
2021-08-09 04:56:58 -0600

    make it better5

--- routes/productos.js
+++ routes/productos.js
@@ -41,8 +41,42 @@ router.post('/nuevo', async(req, res) => {
       })
     }
   });
-
-
+//Eliminar un producto
+  router.delete('/eliminar/:id', async(req, res) => {
+  const _id = req.params.id;
+  try {
+    const notaDb = await productos.findByIdAndDelete({_id});
+    if(!notaDb){
+      return res.status(400).json({
+        mensaje: 'No se encontró el id indicado',
+        error
+      })
+    }
+    res.json(notaDb);  
+  } catch (error) {
+    return res.status(400).json({
+      mensaje: 'Ocurrio un error',
+      error
+    })
+  }
+});
+// Put actualizar una nota
+router.put('/modificar/:id', async(req, res) => {
+    const _id = req.params.id;
+    const body = req.body;
+    try {
+      const notaDb = await productos.findByIdAndUpdate(
+        _id,
+        body,
+        {new: true});
+      res.json(notaDb);  
+    } catch (error) {
+      return res.status(400).json({
+        mensaje: 'Ocurrio un error',
+        error
+      })
+    }
+  });
 
 
 // Exportamos la configuración de express app
